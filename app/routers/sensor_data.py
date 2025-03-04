cbf142b34605b1f4a11ac7184928c2192423075a
Aaron Tummon <aarontummon@gmail.com>
2025-03-04 14:54:42 +0100

    implement query params for getting sensor data

--- app/routers/sensor_data.py
+++ app/routers/sensor_data.py
@@ -1,10 +1,34 @@
-from fastapi import APIRouter
+from datetime import datetime
+from typing import List
+from fastapi import APIRouter, Query
+
+from enum import Enum
+
+
+class Metric(str, Enum):
+    WIND_SPEED = "wind_speed"
+    TEMPERATURE = "temp"
+    HUMIDITY = "humidity"
+
+
+class Statistic(str, Enum):
+    AVERAGE = "avg"
+    MINIMUM = "min"
+    MAXIMUM = "max"
+    SUM = "sum"
+
 
 router = APIRouter()
 
 fake_items_db = [{"sensor_id": "1"}, {"sensor_id": "2"}, {"sensor_id": "3"}]
 
 
-@router.get("/sensor_data/")
-async def read_sensor_data():
+@router.get("/sensor_data")
+async def read_sensor_data(
+    metric: List[Metric] = Query(),
+    statistic: Statistic = Query(),
+    start: datetime | None = Query(None),
+    end: datetime | None = Query(None),
+    sensor_id: List[str] | None = Query(None),
+):
     return fake_items_db
