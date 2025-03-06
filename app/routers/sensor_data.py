54c6a5d6c7957512d87890f7f90cd3bf5aed27bf
Aaron Tummon <aarontummon@gmail.com>
2025-03-06 12:28:56 +0100

    docs for endpoints

--- app/routers/sensor_data.py
+++ app/routers/sensor_data.py
@@ -16,6 +16,20 @@ async def read_sensor_data(
     sensor_data_query: Annotated[SensorDataQuery, Query()],
     sensor_data_service: Annotated[SensorDataService, Depends(get_sensor_data_service)],
 ):
+    """
+    Query a sensor data readings:
+
+    - **sensor_id**: this is a list of UUID4 uuid. Here are some
+        - db858a5c-2b2a-4ddf-967d-5940c4632c5b
+        - 69c10414-2f70-462c-9693-7e761f3b3a76
+        - a4c835f5-91b7-4810-bf3a-b7cfa96dce2e
+        - 18677714-e71e-4cda-bbf4-858f5fe87ab4
+        - 84e5d68f-5e0e-434e-9cf5-c0fbfe42659b
+    - **start_time**: this should be isoformat in utc timezone please
+    - **end_time**: this should be isoformat in utc timezone please
+    - **metric (REQUIRED)**: list of metrics: humidity, temp, wind_speed
+    - **statistic (REQUIRED)**: one statistic: avg, sum, min, max
+    """
     return sensor_data_service.query_sensor_data(sensor_data_query)
 
 
@@ -24,4 +38,17 @@ async def create_sensor_data(
     sensor_data: SensorDataCreate,
     sensor_data_service: Annotated[SensorDataService, Depends(get_sensor_data_service)],
 ):
+    """
+    Create a sensor data reading:
+
+    - **sensor_id**: this should be a UUID4 uuid. Here are some
+        - db858a5c-2b2a-4ddf-967d-5940c4632c5b
+        - 69c10414-2f70-462c-9693-7e761f3b3a76
+        - a4c835f5-91b7-4810-bf3a-b7cfa96dce2e
+        - 18677714-e71e-4cda-bbf4-858f5fe87ab4
+        - 84e5d68f-5e0e-434e-9cf5-c0fbfe42659b
+    - **timestamp**: this should be isoformat in utc timezone please
+    - **metric**: humidity, temp, wind_speed
+    - **value**: float/decimal value
+    """
     return sensor_data_service.create_sensor_data(sensor_data)
