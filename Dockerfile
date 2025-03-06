b7fb1266e5c7bfa6031e637c9555d64c8fef0c14
Aaron Tummon <aarontummon@gmail.com>
2025-03-06 12:29:10 +0100

    run from docker-compose

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.13.0
+
+WORKDIR /urithiru-api
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY app/ app/
+COPY scripts/ scripts/
+EXPOSE 8001
+CMD [ "fastapi", "dev", "--port", "8001" ]
--- scripts/create_dynamodb_table.py
+++ scripts/create_dynamodb_table.py
@@ -2,6 +2,10 @@
 
 import boto3
 import argparse
+import sys
+import os
+
+DYNAMO_DB_TABLE = "WeatherData"
 
 # Default to creating the table locally, but offer the option of creating the
 # table in an instance running in AWS.
@@ -18,15 +22,21 @@ if args.remote:
 else:
     dynamodb = boto3.resource(
         "dynamodb",
-        endpoint_url="http://localhost:8000",
-        aws_access_key_id="fakeKeyId",
-        aws_secret_access_key="fakeAccessKey",
-        region_name="eu-west-1",
+        endpoint_url=os.getenv("AWS_ENDPOINT"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION"),
     )
+
+existing_tables = dynamodb.meta.client.list_tables()["TableNames"]
+
+if DYNAMO_DB_TABLE in existing_tables:
+    print(f"Table '{DYNAMO_DB_TABLE}' already exists.")
+    sys.exit(0)
 
 # Create the DynamoDB table.
 table = dynamodb.create_table(
-    TableName="WeatherData",
+    TableName=DYNAMO_DB_TABLE,
     KeySchema=[
         {"AttributeName": "sensor_id_metric", "KeyType": "HASH"},
         {"AttributeName": "timestamp", "KeyType": "RANGE"},
